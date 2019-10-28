75031f165c625d38a7939e70d39641583dd3535e
adetumi <49404133+adetumi@users.noreply.github.com>
2019-10-28 17:12:55 +0100

    Add files via upload

---
+++ OnlineExamination/examOnline.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="examOnline.aspx.cs" Inherits="OnlineExamination.examOnline" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <center>
+                <h1>How to create An online examination test</h1>
+                <hr />
+                <asp:Repeater ID="Repeater1" runat="server">
+                    <ItemTemplate> 
+                        <table>
+                            <tr>
+
+                                <td>
+                                    <%#Eval("Qid") %> :  <%#Eval("Question") %>
+                                </td>
+
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <asp:RadioButton ID="RadOption1" runat="server" Text =' <%#Eval("option1") %>' GroupName ="rdexam"></asp:RadioButton>
+                                    <asp:RadioButton ID="RadOption2" runat="server" Text =' <%#Eval("option2") %>' GroupName ="rdexam"></asp:RadioButton>
+                                    <asp:RadioButton ID="RadOption3" runat="server" Text =' <%#Eval("option3") %>' GroupName ="rdexam"></asp:RadioButton>
+                                    <asp:RadioButton ID="RadOption4" runat="server" Text =' <%#Eval("option4") %>' GroupName ="rdexam"></asp:RadioButton>
+                                    <br />
+                                    <asp:Label ID="CorrectAns" runat="server" Text='<%#Eval("correctans") %>' Visible ="false"></asp:Label>
+
+                                </td>
+
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <asp:Label ID="SelectedOption" runat="server" Text=""></asp:Label>
+
+                                </td>
+
+                            </tr>
+
+
+                        </table>
+
+                    </ItemTemplate>
+                </asp:Repeater>
+                <asp:Button ID="ButtonSubmit" runat="server" Text="Submit" OnClick="ButtonSubmit_Click"></asp:Button>
+            </center>
+        </div>
+    </form>
+</body>
+</html>
---
+++ OnlineExamination/examOnline.aspx.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace OnlineExamination
+{
+    public partial class examOnline : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+
+                String mainConnection = ConfigurationManager.ConnectionStrings["Myconnection"].ConnectionString;
+                SqlConnection sqlcon = new SqlConnection(mainConnection);
+                string sqlQuery = "select * from onlineExam ";
+                sqlcon.Open();
+
+                SqlCommand sqlcomm = new SqlCommand(sqlQuery, sqlcon);
+                SqlDataAdapter sqlData = new SqlDataAdapter(sqlcomm);
+                DataTable dTable = new DataTable();
+                sqlData.Fill(dTable);
+
+                Repeater1.DataSource = dTable;
+                Repeater1.DataBind();
+
+
+                sqlcon.Close();
+            }
+
+
+
+
+
+        }
+
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            foreach (RepeaterItem ri in Repeater1.Items)
+            {
+                RadioButton radioButton1 = (RadioButton)ri.FindControl("RadOption1");
+                Label CorrectAns = (Label)ri.FindControl("CorrectAns");
+                CorrectAns.Visible = true;
+
+                if (radioButton1.Checked == true)
+                {
+                    if (radioButton1.Text.Equals(CorrectAns.Text))
+                    {
+                        Label selectedAnswer = (Label)ri.FindControl("SelectedOption");
+                        selectedAnswer.Text = "the selected answer is <b>" + radioButton1.Text.ToString() + "</b>";
+                        selectedAnswer.ForeColor = System.Drawing.Color.Green;
+
+                    }
+                    else
+                    {
+                        Label wrongAnswer = (Label)ri.FindControl("SelectedOption");
+                        wrongAnswer.Text = "the selected answer is <b>" + radioButton1.Text.ToString() + "</b> is wrong !";
+                        wrongAnswer.ForeColor = System.Drawing.Color.Red;
+                    }
+
+                }
+            }
+            foreach (RepeaterItem ri in Repeater1.Items)
+            {
+                RadioButton radioButton2 = (RadioButton)ri.FindControl("RadOption2");
+                Label CorrectAns = (Label)ri.FindControl("CorrectAns");
+                CorrectAns.Visible = true;
+
+                if (radioButton2.Checked == true)
+                {
+                    if (radioButton2.Text.Equals(CorrectAns.Text))
+                    {
+                        Label selectedAnswer = (Label)ri.FindControl("SelectedOption");
+                        selectedAnswer.Text = "the selected answer is <b>" + radioButton2.Text.ToString() + "</b>";
+                        selectedAnswer.ForeColor = System.Drawing.Color.Green;
+
+                    }
+                    else
+                    {
+                        Label wrongAnswer = (Label)ri.FindControl("SelectedOption");
+                        wrongAnswer.Text = "the selected answer is <b>" + radioButton2.Text.ToString() + "</b> is wrong !";
+                        wrongAnswer.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+            foreach (RepeaterItem ri in Repeater1.Items)
+            {
+                RadioButton radioButton3 = (RadioButton)ri.FindControl("RadOption3");
+                Label CorrectAns = (Label)ri.FindControl("CorrectAns");
+                CorrectAns.Visible = true;
+
+                if (radioButton3.Checked == true)
+                {
+                    if (radioButton3.Text.Equals(CorrectAns.Text))
+                    {
+                        Label selectedAnswer = (Label)ri.FindControl("SelectedOption");
+                        selectedAnswer.Text = "the selected answer is <b>" + radioButton3.Text.ToString() + "</b>";
+                        selectedAnswer.ForeColor = System.Drawing.Color.Green;
+
+                    }
+                    else
+                    {
+                        Label wrongAnswer = (Label)ri.FindControl("SelectedOption");
+                        wrongAnswer.Text = "the selected answer is <b>" + radioButton3.Text.ToString() + "</b> is wrong !";
+                        wrongAnswer.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+            foreach (RepeaterItem ri in Repeater1.Items)
+            {
+                RadioButton radioButton4 = (RadioButton)ri.FindControl("RadOption4");
+                Label CorrectAns = (Label)ri.FindControl("CorrectAns");
+                CorrectAns.Visible = true;
+
+                if (radioButton4.Checked == true)
+                {
+                    if (radioButton4.Text.Equals(CorrectAns.Text))
+                    {
+                        Label selectedAnswer = (Label)ri.FindControl("SelectedOption");
+                        selectedAnswer.Text = "the selected answer is <b>" + radioButton4.Text.ToString() + "</b>";
+                        selectedAnswer.ForeColor = System.Drawing.Color.Green;
+
+                    }
+                    else
+                    {
+                        Label wrongAnswer = (Label)ri.FindControl("SelectedOption");
+                        wrongAnswer.Text = "the selected answer is <b>" + radioButton4.Text.ToString() + "</b> is wrong !";
+                        wrongAnswer.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+        }
+    }
+}
